6888e4690313044a429b572010090537f475498c
Prangula <Prangula731@gmail.com>
2024-03-06 13:52:25 +0400

    SplashScreen to MainActivity

--- app/src/main/java/com/example/birthdayreminder1/SplashScreenActivity.kt
+++ app/src/main/java/com/example/birthdayreminder1/SplashScreenActivity.kt
@@ -1,11 +1,25 @@
 package com.example.birthdayreminder1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        lifecycleScope.launch{
+
+            delay(3000)
+            val intent = Intent(this@SplashScreenActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
     }
 }
